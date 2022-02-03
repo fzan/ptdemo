afef56517a18c25a6230ae7cc0a792c68a7bf914
Fabio <gtazok@gmail.com>
2022-02-03 15:06:07 +0100

    test

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,12 +2,9 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
-
 export default () => (
   <Card>
-    <h1>Demo dashboard</h1>
-    <CardContent>
-      Demo
-    </CardContent>
+    <h1>Demo</h1>
+    <CardContent>Demo</CardContent>
   </Card>
 );
