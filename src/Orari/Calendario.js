ea56875fef2a46b3efc375a2f5fa26ced5a9000b
LAPTOP-B8SO691L\gianf <gianfelice.buonfini@proietti.it>
2022-02-18 17:52:46 +0100

    OrarioTimbratureTipos (da modificare)

--- src/Orari/Calendario.js
+++ src/Orari/Calendario.js
@@ -5,6 +5,8 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 // import interactionPlugin from '@fullcalendar/interaction'
 import { INITIAL_EVENTS, createEventId } from './event-utils'
 import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
+import Grid from '@material-ui/core/Grid'
+import { withDataProvider } from 'react-admin'
 
 const DataStandard = "2020-01-01"
 
@@ -13,7 +15,7 @@ const events = [
   { title: "USCITA", id: "2", color: "red" }
 ]
 
-export default class DemoApp extends React.Component {
+class DemoApp extends React.Component {
 
   calendarRef = React.createRef();
 
@@ -23,6 +25,11 @@ export default class DemoApp extends React.Component {
   }
 
   componentDidMount() {
+    debugger
+    this.props.dataProvider.getList('orarios', { pagination: { page: 1, perPage: 10000 }, sort: { field: "id", order: "asc" } }).then(data => {
+      console.log(data)
+    })
+
     let draggableEl = document.getElementById("external-events");
     this.calendarRef.current.getApi().gotoDate(DataStandard);
     new Draggable(draggableEl, {
@@ -43,72 +50,99 @@ export default class DemoApp extends React.Component {
 
   render() {
     return (
-      <div className='demo-app'>
-        {this.renderSidebar()}
-        <div
-          id="external-events"
-          style={{
-            padding: "10px",
-            width: "80%",
-            height: "auto",
-            maxHeight: "-webkit-fill-available"
-          }}
-        >
-          <p align="center">
-            <strong> Events</strong>
-          </p>
-          {events.map(event => (
-            <div
-              className="fc-event"
-              title={event.title}
-              data={event.id}
-              key={event.id}
-              style={{ backgroundColor: event.color }}
-              color={event.color}
-              overlap={false}
-            >
-              {event.title}
+      <>
+        <p align="center">
+          <strong>Inserimento Fasce di Giornata</strong>
+        </p>
+        <Grid container spacing={1} style={{ width: "100%" }}>
+          <Grid item xs={2}>
+            <div className='demo-app'>
+              {this.renderSidebar()}
+              <div
+                id="external-events"
+                style={{
+                  padding: "10px",
+                  width: "80%",
+                  height: "auto",
+                  maxHeight: "-webkit-fill-available"
+                }}
+              >
+                <div style={{ backgroundColor: 'white' }}>
+                  <p align="center">
+                    <strong>Eventi</strong>
+                  </p>
+                  {events.map(event => (
+                    <>
+                      <div
+                        className="fc-event"
+                        title={event.title}
+                        data={event.id}
+                        key={event.id}
+                        duration={"01:00"}
+                        style={{ backgroundColor: event.color, borderRadius: '10px', textAlign: 'center', cursor: 'grab', margin: '5px' }}
+                        color={event.color}
+                        overlap={false}
+                      >
+                        {event.title}
+                      </div>
+                    </>
+                  ))}
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: "myCustomButton",
-              // center: "title",
-              right: '' //,timeGridWeek,timeGridDay
-            }}
-            customButtons={{
-              myCustomButton: {
-                text: 'Cambio Fasce Orarie',
-                click: function () {
-                  alert('modifica componente!');
-                },
-              },
-            }}
-            initialView='timeGridDay'
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            select={this.handleDateSelect}
-            // eventContent={renderEventContent} // custom render function (al click)
-            eventClick={this.handleEventClick}
-            // eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            droppable={true}
-            ref={this.calendarRef}
-          /* you can update a remote database when these fire:
-          eventAdd={function(){}}
-          eventChange={function(){}}
-          eventRemove={function(){}}
-          */
-          />
-        </div>
-      </div>
+          </Grid>
+          <Grid item xs={10}>
+            <div className='demo-app-main'>
+              <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                  left: "myCustomButton",
+                  // center: "title",
+                  right: '' //,timeGridWeek,timeGridDay
+                }}
+                customButtons={{
+                  myCustomButton: {
+                    text: 'Cambio Fasce Orarie',
+                    click: function () {
+                      alert('modifica componente!');
+                    },
+                  },
+                }}
+                initialView='timeGridDay'
+                editable={true}
+                selectable={true}
+                selectMirror={false}
+                dayMaxEvents={true}
+                // weekends={this.state.weekendsVisible}
+                initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+                select={this.handleDateSelect}
+                // eventContent={renderEventContent} // custom render function (al click)
+                eventClick={this.handleEventClick}
+                eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+                droppable={true}
+                ref={this.calendarRef}
+                allDaySlot={false}
+                dayHeaders={false}
+                defaultTimedEventDuration={'01:00:00'}
+                forceEventDuration={true}
+                select={null}
+              // eventAdd={this.handleDateSelect}
+              /* you can update a remote database when these fire:
+              eventAdd={function () { }}
+              eventChange={function () { }}
+              eventRemove={function () { }}
+              */
+              />
+            </div>
+          </Grid>
+          <div className='demo-app-sidebar-section'>
+            <h2>Eventi Inseriti ({this.state.currentEvents.length})</h2>
+            <ul>
+              {this.state.currentEvents.map(renderSidebarEvent)}
+            </ul>
+          </div>
+        </Grid>
+      </>
     )
   }
 
@@ -116,27 +150,10 @@ export default class DemoApp extends React.Component {
     return (
       <div className='demo-app-sidebar'>
         <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
+          <h2>Istruzioni</h2>
           <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>
-            {this.state.currentEvents.map(renderSidebarEvent)}
+            <li>Puoi spostare e ricalibrare la fascia oraria</li>
+            <li>Click su un evento per poterlo eliminare</li>
           </ul>
         </div>
       </div>
@@ -144,6 +161,7 @@ export default class DemoApp extends React.Component {
   }
 
   handleWeekendsToggle = () => {
+
     this.setState({
       weekendsVisible: !this.state.weekendsVisible
     })
@@ -156,6 +174,7 @@ export default class DemoApp extends React.Component {
     calendarApi.unselect() // clear date selection
 
     if (title) {
+
       calendarApi.addEvent({
         id: createEventId(),
         title,
@@ -191,8 +210,13 @@ function renderEventContent(eventInfo) {
 function renderSidebarEvent(event) {
   return (
     <li key={event.id}>
-      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
       <i>{event.title}</i>
+      <b> Dalle {formatDate(event.start, { hour: 'numeric', minute: 'numeric' })}</b>
+
+      <b> alle {formatDate(event.end, { hour: 'numeric', minute: 'numeric' })}</b>
+      {/* <b> Alle {formatDate(event.start.getTime() + (60 * 60 * 1000), { hour: 'numeric', minute: 'numeric' })}</b>} */}
     </li>
   )
 }
+
+export default withDataProvider(DemoApp)
